98a7d15a109d64a6c09cb1d5d1194045204f5af5
lichenghang <lichenghang@babytree-inc.com>
2018-11-19 13:53:45 +0800

    mvp usecase add.

---
+++ netkits/src/main/java/com/lch/netkit/common/mvc/UseCase.java
@@ -0,0 +1,41 @@
+package com.lch.netkit.common.mvc;
+
+public abstract class UseCase<P, R> {
+    private static final TaskExecutor def = new DefaultTaskExecutor();
+
+    public TaskExecutor getTaskExecutor() {
+        return def;
+    }
+
+    public void invokeAsync(final P params, final ControllerCallback<R> result) {
+
+        getTaskExecutor().executeOnDiskIO(new Runnable() {
+            @Override
+            public void run() {
+
+                final ResponseValue<R> r = execute(params);
+
+                getTaskExecutor().executeOnMainThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (r.hasError()) {
+                            result.onError(r.code, r.getErrorMsg());
+                        } else {
+                            result.onSuccess(r.data);
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+
+    public ResponseValue<R> invokeSync(P params) {
+        return execute(params);
+    }
+
+
+    protected abstract ResponseValue<R> execute(P parameters);
+
+}
