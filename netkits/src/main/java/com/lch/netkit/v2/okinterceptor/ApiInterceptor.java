0068ea56940bf5f6a923f32c5d5e709a67b246b1
lichenghang <lichenghang@babytree-inc.com>
2020-06-28 14:28:05 +0800

    统一请求参数

--- netkits/src/main/java/com/lch/netkit/v2/okinterceptor/ApiInterceptor.java
+++ netkits/src/main/java/com/lch/netkit/v2/okinterceptor/ApiInterceptor.java
@@ -7,9 +7,21 @@ import com.lch.netkit.v2.apirequest.ApiRequestParams;
 
 public interface ApiInterceptor {
 
+    /**
+     * 对参数进行处理，比如添加公共参数等。
+     *
+     * @param requestParams
+     * @return
+     */
     @NonNull
     ApiRequestParams interceptApiRequestParams(@NonNull ApiRequestParams requestParams);
 
+    /**
+     * 对响应内容进行处理。
+     *
+     * @param responseString
+     * @return
+     */
     String interceptResponse(String responseString);
 
 }
